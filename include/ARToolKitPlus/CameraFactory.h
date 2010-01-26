dd941115b35e83d423a44a98ef8c7d532ed5af8f
Pavel Rojtberg <pavel@rojtberg.net>
2010-01-26 13:25:36 +0100

    formatting

--- include/ARToolKitPlus/CameraFactory.h
+++ include/ARToolKitPlus/CameraFactory.h
@@ -1,42 +1,41 @@
 /* ========================================================================
-* PROJECT: ARToolKitPlus
-* ========================================================================
-* This work is based on the original ARToolKit developed by
-*   Hirokazu Kato
-*   Mark Billinghurst
-*   HITLab, University of Washington, Seattle
-* http://www.hitl.washington.edu/artoolkit/
-*
-* Copyright of the derived and new portions of this work
-*     (C) 2006 Graz University of Technology
-*
-* This framework is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This framework is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this framework; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-* For further information please contact 
-*   Dieter Schmalstieg
-*   <schmalstieg@icg.tu-graz.ac.at>
-*   Graz University of Technology, 
-*   Institut for Computer Graphics and Vision,
-*   Inffeldgasse 16a, 8010 Graz, Austria.
-* ========================================================================
-** @author   Thomas Pintaric
-*
-* $Id$
-* @file
-* ======================================================================== */
-
+ * PROJECT: ARToolKitPlus
+ * ========================================================================
+ * This work is based on the original ARToolKit developed by
+ *   Hirokazu Kato
+ *   Mark Billinghurst
+ *   HITLab, University of Washington, Seattle
+ * http://www.hitl.washington.edu/artoolkit/
+ *
+ * Copyright of the derived and new portions of this work
+ *     (C) 2006 Graz University of Technology
+ *
+ * This framework is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this framework; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * For further information please contact
+ *   Dieter Schmalstieg
+ *   <schmalstieg@icg.tu-graz.ac.at>
+ *   Graz University of Technology,
+ *   Institut for Computer Graphics and Vision,
+ *   Inffeldgasse 16a, 8010 Graz, Austria.
+ * ========================================================================
+ ** @author   Thomas Pintaric
+ *
+ * $Id$
+ * @file
+ * ======================================================================== */
 
 #ifndef __ARTOOLKIT_CAMERAFACTORY_HEADERFILE__
 #define __ARTOOLKIT_CAMERAFACTORY_HEADERFILE__
@@ -45,12 +44,11 @@
 
 namespace ARToolKitPlus {
 
-class CameraFactory
-{
-	public:
-		Camera *createCamera(const char* filename);
+class CameraFactory {
+public:
+	Camera *createCamera(const char* filename);
 };
 
-}  // namespace ARToolKitPlus
+} // namespace ARToolKitPlus
 
 #endif // __ARTOOLKIT_CAMERAFACTORY_HEADERFILE__
