7d625a3bd62417a4f6c6c4cf16edb7360f73a038
Pavel Rojtberg <pavel@rojtberg.net>
2010-02-08 02:07:18 +0100

    * clean up examples

--- sample/multi/main.cpp
+++ sample/multi/main.cpp
@@ -1,61 +1,54 @@
-/*
-    Copyright (C) 2010  ARToolkitPlus Authors
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Authors:
-      Daniel Wagner
-      Pavel Rojtberg
+/**
+ * Copyright (C) 2010  ARToolkitPlus Authors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *  Daniel Wagner
+ *  Pavel Rojtberg
  */
 
-
-//
 // Simple example to demonstrate multi-marker tracking with ARToolKitPlus
 // This sample does not open any graphics window. It just
 // loads test images and shows use to use the ARToolKitPlus API.
-//
 
 #include "ARToolKitPlus/TrackerMultiMarkerImpl.h"
 
+using ARToolKitPlus::TrackerMultiMarker;
+using ARToolKitPlus::TrackerMultiMarkerImpl;
 
-int main(int argc, char** argv)
-{
-    const int     width = 320, height = 240, bpp = 1;
-    size_t        numPixels = width*height*bpp;
-    size_t        numBytesRead;
-    const char    *fName = "data/markerboard_480-499.raw";
+int main(int argc, char** argv) {
+    const int width = 320, height = 240, bpp = 1;
+    size_t numPixels = width * height * bpp;
+    size_t numBytesRead;
+    const char *fName = "data/markerboard_480-499.raw";
     unsigned char *cameraBuffer = new unsigned char[numPixels];
 
     // try to load a test camera image.
     // these images files are expected to be simple 8-bit raw pixel
     // data without any header. the images are expetected to have a
     // size of 320x240.
-    //
-    if(FILE* fp = fopen(fName, "rb"))
-    {
+    if (FILE* fp = fopen(fName, "rb")) {
         numBytesRead = fread(cameraBuffer, 1, numPixels, fp);
         fclose(fp);
-    }
-    else
-    {
+    } else {
         printf("Failed to open %s\n", fName);
         delete cameraBuffer;
         return -1;
     }
 
-    if(numBytesRead != numPixels)
-    {
+    if (numBytesRead != numPixels) {
         printf("Failed to read %s\n", fName);
         delete cameraBuffer;
         return -1;
@@ -67,22 +60,21 @@ int main(int argc, char** argv)
     //  - works with luminance (gray) images
     //  - can load a maximum of 1 pattern
     //  - can detect a maximum of 8 patterns in one image
-    ARToolKitPlus::TrackerMultiMarker *tracker = new ARToolKitPlus::TrackerMultiMarkerImpl(width,height);
+    TrackerMultiMarker *tracker = new TrackerMultiMarkerImpl(width, height, 6, 6, 6, 1, 8);
 
-	tracker->setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
+    tracker->setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
 
     // load a camera file.
-	if(!tracker->init("data/PGR_M12x0.5_2.5mm.cal", "data/markerboard_480-499.cfg", 1.0f, 1000.0f))
-	{
-		printf("ERROR: init() failed\n");
-		delete cameraBuffer;
-		delete tracker;
-		return -1;
-	}
+    if (!tracker->init("data/PGR_M12x0.5_2.5mm.cal", "data/markerboard_480-499.cfg", 1.0f, 1000.0f)) {
+        printf("ERROR: init() failed\n");
+        delete cameraBuffer;
+        delete tracker;
+        return -1;
+    }
 
-	tracker->getCamera()->printSettings();
+    tracker->getCamera()->printSettings();
 
-	// the marker in the BCH test image has a thiner border...
+    // the marker in the BCH test image has a thiner border...
     tracker->setBorderWidth(0.125f);
 
     // set a threshold. we could also activate automatic thresholding
@@ -98,8 +90,8 @@ int main(int argc, char** argv)
     // switch to simple ID based markers
     // use the tool in tools/IdPatGen to generate markers
     tracker->setMarkerMode(ARToolKitPlus::MARKER_ID_SIMPLE);
-    
-    // switch one the hull only tracking
+
+    // switch on the hull only tracking
     tracker->setHullMode(ARToolKitPlus::HULL_FOUR);
 
     // do the OpenGL camera setup
@@ -113,31 +105,28 @@ int main(int argc, char** argv)
     //glMatrixMode(GL_MODELVIEW)
     //glLoadMatrixf(tracker->getModelViewMatrix());
 
-	printf("\n%d good Markers found and used for pose estimation.\nPose-Matrix:\n  ", numDetected);
-	for(int i=0; i<16; i++)
-		printf("%.2f  %s", tracker->getModelViewMatrix()[i], (i%4==3)?"\n  " : "");
+    printf("\n%d good Markers found and used for pose estimation.\nPose-Matrix:\n  ", numDetected);
+    for (int i = 0; i < 16; i++)
+        printf("%.2f  %s", tracker->getModelViewMatrix()[i], (i % 4 == 3) ? "\n  " : "");
 
-	bool showConfig = false;
+    bool showConfig = false;
 
-	if(showConfig)
-	{
-		const ARToolKitPlus::ARMultiMarkerInfoT *artkpConfig = tracker->getMultiMarkerConfig();
-		printf("%d markers defined in multi marker cfg\n", artkpConfig->marker_num);
+    if (showConfig) {
+        const ARToolKitPlus::ARMultiMarkerInfoT *artkpConfig = tracker->getMultiMarkerConfig();
+        printf("%d markers defined in multi marker cfg\n", artkpConfig->marker_num);
 
-		printf("marker matrices:\n");
-		for(int multiMarkerCounter = 0; multiMarkerCounter < artkpConfig->marker_num; multiMarkerCounter++)
-		{
-			printf("marker %d, id %d:\n", multiMarkerCounter, artkpConfig->marker[multiMarkerCounter].patt_id);
-			for(int row = 0; row < 3; row++)
-			{
-				for(int column = 0; column < 4; column++)
-					printf("%.2f  ", artkpConfig->marker[multiMarkerCounter].trans[row][column]);
-				printf("\n");
-			}
-		}
-	}
+        printf("marker matrices:\n");
+        for (int multiMarkerCounter = 0; multiMarkerCounter < artkpConfig->marker_num; multiMarkerCounter++) {
+            printf("marker %d, id %d:\n", multiMarkerCounter, artkpConfig->marker[multiMarkerCounter].patt_id);
+            for (int row = 0; row < 3; row++) {
+                for (int column = 0; column < 4; column++)
+                    printf("%.2f  ", artkpConfig->marker[multiMarkerCounter].trans[row][column]);
+                printf("\n");
+            }
+        }
+    }
 
-    delete [] cameraBuffer;
-	delete tracker;
-	return 0;
+    delete[] cameraBuffer;
+    delete tracker;
+    return 0;
 }
--- sample/simple/main.cpp
+++ sample/simple/main.cpp
@@ -19,110 +19,107 @@
  *  Pavel Rojtberg
  */
 
-//
 // Simple example to demonstrate usage of ARToolKitPlus
 // This sample does not open any graphics window. It just
 // loads test images and shows how to use the ARToolKitPlus API.
-//
 
 #include "ARToolKitPlus/TrackerSingleMarkerImpl.h"
 
+using ARToolKitPlus::TrackerSingleMarker;
+using ARToolKitPlus::TrackerSingleMarkerImpl;
+
 int main(int argc, char** argv) {
-	// switch this between true and false to test
-	// simple-id versus BCH-id markers
-	//
-	const bool useBCH = false;
+    // switch this between true and false to test
+    // simple-id versus BCH-id markers
+    const bool useBCH = false;
 
-	const int width = 320, height = 240, bpp = 1;
-	size_t numPixels = width * height * bpp;
-	size_t numBytesRead;
-	const char *fName = useBCH ? "data/image_320_240_8_marker_id_bch_nr0100.raw"
-			: "data/image_320_240_8_marker_id_simple_nr031.raw";
-	unsigned char *cameraBuffer = new unsigned char[numPixels];
+    const int width = 320, height = 240, bpp = 1;
+    size_t numPixels = width * height * bpp;
+    size_t numBytesRead;
+    const char *fName = useBCH ? "data/image_320_240_8_marker_id_bch_nr0100.raw"
+            : "data/image_320_240_8_marker_id_simple_nr031.raw";
+    unsigned char *cameraBuffer = new unsigned char[numPixels];
 
-	// try to load a test camera image.
-	// these images files are expected to be simple 8-bit raw pixel
-	// data without any header. the images are expetected to have a
-	// size of 320x240.
-	//
-	if (FILE* fp = fopen(fName, "rb")) {
-		numBytesRead = fread(cameraBuffer, 1, numPixels, fp);
-		fclose(fp);
-	} else {
-		printf("Failed to open %s\n", fName);
-		delete cameraBuffer;
-		return -1;
-	}
+    // try to load a test camera image.
+    // these images files are expected to be simple 8-bit raw pixel
+    // data without any header. the images are expetected to have a
+    // size of 320x240.
+    if (FILE* fp = fopen(fName, "rb")) {
+        numBytesRead = fread(cameraBuffer, 1, numPixels, fp);
+        fclose(fp);
+    } else {
+        printf("Failed to open %s\n", fName);
+        delete cameraBuffer;
+        return -1;
+    }
 
-	if (numBytesRead != numPixels) {
-		printf("Failed to read %s\n", fName);
-		delete cameraBuffer;
-		return -1;
-	}
+    if (numBytesRead != numPixels) {
+        printf("Failed to read %s\n", fName);
+        delete cameraBuffer;
+        return -1;
+    }
 
-	// create a tracker that does:
-	//  - 6x6 sized marker images
-	//  - samples at a maximum of 6x6
-	//  - works with luminance (gray) images
-	//  - can load a maximum of 1 pattern
-	//  - can detect a maximum of 8 patterns in one image
-	ARToolKitPlus::TrackerSingleMarker *tracker = new ARToolKitPlus::TrackerSingleMarkerImpl(width, height, 6, 6, 6, 1,
-			8);
+    // create a tracker that does:
+    //  - 6x6 sized marker images
+    //  - samples at a maximum of 6x6
+    //  - works with luminance (gray) images
+    //  - can load a maximum of 1 pattern
+    //  - can detect a maximum of 8 patterns in one image
+    TrackerSingleMarker* tracker = new TrackerSingleMarkerImpl(width, height, 6, 6, 6, 1, 8);
 
-	tracker->setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
-	//tracker->setLoadUndistLUT(true);
+    tracker->setPixelFormat(ARToolKitPlus::PIXEL_FORMAT_LUM);
+    //tracker->setLoadUndistLUT(true);
 
-	// load a camera file.
-	if (!tracker->init("data/PGR_M12x0.5_2.5mm.cal", 1.0f, 1000.0f)) // load MATLAB file
-	{
-		printf("ERROR: init() failed\n");
-		delete cameraBuffer;
-		delete tracker;
-		return -1;
-	}
+    // load a camera file.
+    if (!tracker->init("data/PGR_M12x0.5_2.5mm.cal", 1.0f, 1000.0f)) // load MATLAB file
+    {
+        printf("ERROR: init() failed\n");
+        delete cameraBuffer;
+        delete tracker;
+        return -1;
+    }
 
-	tracker->getCamera()->printSettings();
+    tracker->getCamera()->printSettings();
 
-	// define size of the marker in OpenGL units
-	tracker->setPatternWidth(2.0);
+    // define size of the marker in OpenGL units
+    tracker->setPatternWidth(2.0);
 
-	// the marker in the BCH test image has a thin border...
-	tracker->setBorderWidth(useBCH ? 0.125 : 0.25);
+    // the marker in the BCH test image has a thin border...
+    tracker->setBorderWidth(useBCH ? 0.125 : 0.25);
 
-	// set a threshold. alternatively we could also activate automatic thresholding
-	tracker->setThreshold(150);
+    // set a threshold. alternatively we could also activate automatic thresholding
+    tracker->setThreshold(150);
 
-	// let's use lookup-table undistortion for high-speed
-	// note: LUT only works with images up to 1024x1024
-	tracker->setUndistortionMode(ARToolKitPlus::UNDIST_LUT);
+    // let's use lookup-table undistortion for high-speed
+    // note: LUT only works with images up to 1024x1024
+    tracker->setUndistortionMode(ARToolKitPlus::UNDIST_LUT);
 
-	// RPP is more robust than ARToolKit's standard pose estimator
-	tracker->setPoseEstimator(ARToolKitPlus::POSE_ESTIMATOR_RPP);
+    // RPP is more robust than ARToolKit's standard pose estimator
+    tracker->setPoseEstimator(ARToolKitPlus::POSE_ESTIMATOR_RPP);
 
-	// switch to simple ID based markers
-	// use the tool in tools/IdPatGen to generate markers
-	tracker->setMarkerMode(useBCH ? ARToolKitPlus::MARKER_ID_BCH : ARToolKitPlus::MARKER_ID_SIMPLE);
+    // switch to simple ID based markers
+    // use the tool in tools/IdPatGen to generate markers
+    tracker->setMarkerMode(useBCH ? ARToolKitPlus::MARKER_ID_BCH : ARToolKitPlus::MARKER_ID_SIMPLE);
 
-	// do the OpenGL camera setup
-	//glMatrixMode(GL_PROJECTION)
-	//glLoadMatrixf(tracker->getProjectionMatrix());
+    // do the OpenGL camera setup
+    //glMatrixMode(GL_PROJECTION)
+    //glLoadMatrixf(tracker->getProjectionMatrix());
 
-	// here we go, just two calls to find the camera pose
-	vector<int> markerId = tracker->calc(cameraBuffer);
-	tracker->selectBestMarkerByCf();
-	float conf = (float) tracker->getConfidence();
+    // here we go, just two calls to find the camera pose
+    vector<int> markerId = tracker->calc(cameraBuffer);
+    tracker->selectBestMarkerByCf();
+    float conf = (float) tracker->getConfidence();
 
-	// use the result of calc() to setup the OpenGL transformation
-	//glMatrixMode(GL_MODELVIEW)
-	//glLoadMatrixf(tracker->getModelViewMatrix());
+    // use the result of calc() to setup the OpenGL transformation
+    //glMatrixMode(GL_MODELVIEW)
+    //glLoadMatrixf(tracker->getModelViewMatrix());
 
+    printf("\n\nFound marker %d  (confidence %d%%)\n\nPose-Matrix:\n  ", markerId[0], (int(conf * 100.0f)));
+    for (int i = 0; i < 16; i++)
+        printf("%.2f  %s", tracker->getModelViewMatrix()[i], (i % 4 == 3) ? "\n  " : "");
 
-	printf("\n\nFound marker %d  (confidence %d%%)\n\nPose-Matrix:\n  ", markerId[0], (int(conf * 100.0f)));
-	for (int i = 0; i < 16; i++)
-		printf("%.2f  %s", tracker->getModelViewMatrix()[i], (i % 4 == 3) ? "\n  " : "");
+    delete[] cameraBuffer;
+    delete tracker;
 
-	delete[] cameraBuffer;
-	delete tracker;
-
-	return 0;
+    return 0;
 }
